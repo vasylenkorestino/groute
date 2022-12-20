5c77428e734fc53d8aed82ab266d19060e5e9c93
orestvasylenko <orest.v@peeklogic.com>
2022-12-21 01:44:43 +0200

    groute v1

--- client/src/components/Forms/Form.jsx
+++ client/src/components/Forms/Form.jsx
@@ -74,7 +74,7 @@ const InputForm = ({ endpoint, columns, close, title, mode }) => {
                 { 
                     imageUrl 
                     ?
-                        <img style={{ width:100, height: 150 }} src={imageUrl} alt="routeImage"/>
+                        <img style={{ width:200, height: 400 }} src={imageUrl} alt="routeImage"/>
                     :
                     <>
                         <InputGroup.Button onClick={handleChangeCameraView}><BsFillCameraFill /></InputGroup.Button>
