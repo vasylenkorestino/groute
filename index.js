ec4b01532a9c703de1dd450c1d59aaeb00c6936d
orestvasylenko <orest.v@peeklogic.com>
2022-12-20 21:25:17 +0200

    groute v1

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: true} ))
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+//app.use(express.static(path.join(__dirname, 'client/build')));
+/*
 app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/users', require('./routes/users.route'))
 app.use('/api/salesforce', require('./routes/salesforce.route'))
-
+*/
 app.get('*', (req, res) => {
   //res.sendFile(path.join(__dirname + '/client/build/index.html'));
   res.send('test')
