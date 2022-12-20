c4c84c5077b5182ab498dcce941fef74f97141ab
orestvasylenko <orest.v@peeklogic.com>
2022-12-20 21:23:16 +0200

    groute v1

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { restart } = require('nodemon');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ extended: true} ))
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/api/auth', require('./routes/auth.route'))
+app.use('/api/users', require('./routes/users.route'))
+app.use('/api/salesforce', require('./routes/salesforce.route'))
+
+app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.send('test')
+})
+
+const launch = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://gstarroutes:xnuHZq2bHEX@cluster0.t8er0.mongodb.net/gsroutes?retryWrites=true&w=majority', {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        }, error => { 
+            if(error) throw error
+            console.log('Connected to MongoDB')
+        })
+
+        app.listen(PORT, () => { console.log(`Example app listening on port ${PORT}`) })
+    } catch(error){
+        console.log(error)
+    }
+}
+
+launch()
