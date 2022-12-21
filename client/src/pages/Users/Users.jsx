6669af107f97ced8dcfb47b3069a3ef736cfbc4b
orestvasylenko <orest.v@peeklogic.com>
2022-12-21 21:31:13 +0200

    groute v1

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -12,13 +12,12 @@ const Routes = () => {
         getDrivers()
     }, [])
 
-    //        { name: 'Password', apiName: 'password', type: 'password', width: 100, order: 4, modalOrder: 4, hideCell: true },
-
     const endpoint = '/api/users'
     const columns = [
         { name: 'Name', apiName: 'driverName', type: 'name', width: 400, order: 1, modalOrder: 1, hideCell: false }, 
         { name: 'Email', apiName: 'email', type: 'text', width: 300, order: 2, modalOrder: 2, hideCell: false },
         { name: 'User Name', apiName: 'username', type: 'text', width: 300, order: 3, modalOrder: 3, hideCell: true },  
+        { name: 'Password', apiName: 'password', type: 'password', width: 100, order: 4, modalOrder: 4, hideCell: true },
         { name: 'Salesforce User', apiName: 'sfUser', type: 'select', width: 100, order: 5, modalOrder: 5, hideCell: true, data: drivers.map(driver => ({ label: driver.Name, value: driver.Id })) }, 
         { name: 'isActive', apiName: 'isActive', type: 'checkbox', width: 100, order: 6, modalOrder: 6, hideCell: false }, 
         { name: 'isAdmin', apiName: 'isAdmin', type: 'checkbox', width: 100, order: 7, modalOrder: 7, hideCell: false }
