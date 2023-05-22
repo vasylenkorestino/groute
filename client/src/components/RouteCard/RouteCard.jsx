f5bcb3216a44e0affbf1b1e8324535dd02eef4b3
orestvasylenko <orest.v@peeklogic.com>
2023-05-22 14:05:18 +0300

    service types

--- client/src/components/RouteCard/RouteCard.jsx
+++ client/src/components/RouteCard/RouteCard.jsx
@@ -43,7 +43,9 @@ const RouteCard = ({ route, endpoint, columns, reload }) => {
                                 <div className="lead mb-0"> Comment: { route.Notes__c } </div>
                                 <div className="lead mb-0"> Gallons: { route.Gallons_Collected__c } </div>
                                 <div className="lead mb-0"> Service Type: { route.ServiceType__c } </div>
-                                <div className="lead mb-0"> Service Sub Type: { route.ServiceSubType__c } </div>
+                                { 
+                                    route.ServiceSubType__c && <div className="lead mb-0"> Service Sub Type: { route.ServiceSubType__c } </div>
+                                }
                                 <div className="lead mb-0"> ContainerSize: { route.Container_Size__c } </div>
                             </div>
                         </div></Col>
