782bdfa06edf417fdf7033c2a23d602029a65e1f
Orest Vasylenko <orestvasylenko@MacBook-Pro-Orest.local>
2024-08-26 16:31:01 +0200

    map updates v7

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -18,56 +18,58 @@ const Navigation = () => {
     const { logout, isLogin, isAdmin } = useContext(AuthContext)
 
     return (
-        <Navbar bg="#292d33" variant="dark">
-            <Container>
-            <div style={{ flex: 1, height: 100, alignItems: 'center' }} className='d-flex justify-content-between'>
-                <div style={{ flex: 0.9 }}>
-                    <Navbar.Brand href="/">
-                        <IconContext.Provider value={{ size: "4em" }}>
-                            < GiFoodTruck />
-                        </IconContext.Provider>
-                    </Navbar.Brand>
-                </div>
-    
-                <div style={{ flex: 0.1 }}>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                                    {
-                                        isLogin 
-                                        ? 
-                                            <Nav className="me-auto">
-                                                { 
-                                                    isAdmin 
-                                                    && 
-                                                    <Nav.Link href="/account">
+        <div style={{ width: '100%' }}>
+            <Navbar bg="#292d33" variant="dark">
+                <Container>
+                <div style={{ flex: 1, width: '100%', height: 100, alignItems: 'center' }} className='d-flex justify-content-between'>
+                    <div style={{ flex: 0.9 }}>
+                        <Navbar.Brand href="/">
+                            <IconContext.Provider value={{ size: "4em" }}>
+                                < GiFoodTruck />
+                            </IconContext.Provider>
+                        </Navbar.Brand>
+                    </div>
+        
+                    <div style={{ flex: 0.1 }}>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Navbar.Text>
+                                        {
+                                            isLogin 
+                                            ? 
+                                                <Nav className="me-auto">
+                                                    { 
+                                                        isAdmin 
+                                                        && 
+                                                        <Nav.Link href="/account">
+                                                            <IconContext.Provider value={{ size: "3em" }}>
+                                                                < BiUserCircle />
+                                                            </IconContext.Provider>
+                                                        </Nav.Link>
+                                                    }
+                                                    
+                                                    <Nav.Link href="/" onClick={ logout }>
                                                         <IconContext.Provider value={{ size: "3em" }}>
-                                                            < BiUserCircle />
+                                                            < IoMdLogOut />
                                                         </IconContext.Provider>
                                                     </Nav.Link>
-                                                }
-                                                
-                                                <Nav.Link href="/" onClick={ logout }>
-                                                    <IconContext.Provider value={{ size: "3em" }}>
-                                                        < IoMdLogOut />
-                                                    </IconContext.Provider>
-                                                </Nav.Link>
-                                            </Nav>
-                                        :
-                                            <Nav className="me-auto">
-                                                <Nav.Link href="/login">
-                                                    <IconContext.Provider value={{ size: "3em" }}>
-                                                        < IoMdLogIn />
-                                                    </IconContext.Provider>
-                                                </Nav.Link>
-                                            </Nav>
-                                    }
-                        </Navbar.Text>
-                    </Navbar.Collapse>
+                                                </Nav>
+                                            :
+                                                <Nav className="me-auto">
+                                                    <Nav.Link href="/login">
+                                                        <IconContext.Provider value={{ size: "3em" }}>
+                                                            < IoMdLogIn />
+                                                        </IconContext.Provider>
+                                                    </Nav.Link>
+                                                </Nav>
+                                        }
+                            </Navbar.Text>
+                        </Navbar.Collapse>
+                    </div>
+                    
                 </div>
-                
-            </div>
-        </Container>
-        </Navbar>
+            </Container>
+            </Navbar>
+        </div>
     );
 }
 
