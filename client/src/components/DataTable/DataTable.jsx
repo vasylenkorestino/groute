b5e1379f744679febe5f513099fa4a5c692bf7e1
Orest Vasylenko <orestvasylenko@MacBook-Pro-Orest.local>
2024-08-26 14:29:42 +0200

    map updates v7

--- client/src/components/DataTable/DataTable.jsx
+++ client/src/components/DataTable/DataTable.jsx
@@ -20,6 +20,8 @@ import CheckboxCell from './components/CheckboxCell'
 
 import InputForm from '../Forms/Form'
 
+import { isMobile } from 'react-device-detect'
+
 const DataTable = ({ endpoint, columns }) => {
 
     const { records, setRecord, setRecords, getAllRecords, isNew, setIsNew, setIsReady, loading } = useContext(DataContext)
@@ -79,12 +81,12 @@ const DataTable = ({ endpoint, columns }) => {
             <ModalWindow show={ show } close={ handleClose } context={ <InputForm endpoint={ endpoint } columns={ columns } close={ handleClose } title={ isNew ? 'New User' : 'Edit User'} mode='mongodb'/>} />
             <Container>
                 <Header>
-                    <div style={{ width: '100%', display: 'flex', margin: '20px 10px'}}>
+                    <div style={{ width: isMobile ? '85%' : '100%', display: 'flex', margin: '20px 10px'}}>
                         <div>
                             <IconButton style={{ margin: '0px 10px' }} icon={<PlusIcon />} onClick={ addNew }>Add</IconButton>
                         </div>
-                        <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
-                            <InputGroup inside style={{ width: 400,  }}>
+                        <div style={{ width: isMobile ? '85%' : '100%', display: 'flex', justifyContent: 'flex-end' }}>
+                            <InputGroup inside style={{ width: !isMobile ? 400 : 200 }}>
                                 <Input name='name' placeholder='Type product name...' onChange={ handleFilterParams } />
                                 <InputGroup.Button>
                                 <SearchIcon />
@@ -95,15 +97,24 @@ const DataTable = ({ endpoint, columns }) => {
                     <hr></hr>
                 </Header>
                 <Content>
-                    <Table height={300} data={ records } loading={ !loading }>
+                    <div style={{ width: isMobile ? '85%' : '100%', margin: 20 }}>
+                        <Table height={300} data={ records } loading={ !loading }>
 
-                    { columns.sort((a, b) => { return a.order - b.order }).map(column => ( !column.hideCell && getCell(column) )) }
-
-                    <Column width={100} resizable>
-                        <HeaderCell>Action</HeaderCell>
-                        <ActionCell dataKey="_id" setShow={ setShow } endpoint={ endpoint }/>
-                    </Column>
-                    </Table>
+                        { columns.sort((a, b) => { return a.order - b.order }).map(column => {
+                            if(isMobile){ 
+                                if(column.name == 'Name'){
+                                    return getCell(column)
+                                }
+                            } else {
+                                return !column.hideCell && getCell(column) 
+                            }
+                        }) }
+                        <Column width={100} resizable>
+                            <HeaderCell>Action</HeaderCell>
+                            <ActionCell dataKey="_id" setShow={ setShow } endpoint={ endpoint }/>
+                        </Column>
+                        </Table>
+                    </div>
                 </Content>
             </Container>
         </div>
--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -14,8 +14,8 @@ const Routes = () => {
 
     const endpoint = '/api/users'
     const columns = [
-        { name: 'Name', apiName: 'driverName', type: 'name', width: 400, order: 1, modalOrder: 1, hideCell: false }, 
-        { name: 'Email', apiName: 'email', type: 'text', width: 300, order: 2, modalOrder: 2, hideCell: false },
+        { name: 'Name', apiName: 'driverName', type: 'name', width: 220, order: 1, modalOrder: 1, hideCell: false }, 
+        { name: 'Email', apiName: 'email', type: 'text', width: 840, order: 2, modalOrder: 2, hideCell: false },
         { name: 'User Name', apiName: 'username', type: 'text', width: 300, order: 3, modalOrder: 3, hideCell: true },  
         { name: 'Password', apiName: 'password', type: 'password', width: 100, order: 4, modalOrder: 4, hideCell: true },
         { name: 'Salesforce User', apiName: 'sfUser', type: 'select', width: 100, order: 5, modalOrder: 5, hideCell: true, data: drivers.map(driver => ({ label: driver.Name, value: driver.Id })) }, 
@@ -24,8 +24,8 @@ const Routes = () => {
     ]
 
     return (
-        <div style={{ width: 1100 }} className="d-flex justify-content-center">
-            <div style={{ width: 1000 }}>
+        <div style={{ marginLeft: 30, width: '95%', height: '100%' }} className="d-flex justify-content-center">
+            <div style={{ width: '100%', height: '100%' }}>
                 <DataTable endpoint={ endpoint } columns={ columns }/>
             </div>
         </div>
